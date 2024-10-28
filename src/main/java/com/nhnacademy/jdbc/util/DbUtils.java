8ba0b38a5cc9aa6743fea4e09ae435db6a199f2a
Que-Sera-Sera-Today <30decade@gmail.com>
2024-10-28 13:06:53 +0900

    Add files via upload

---
+++ src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
@@ -0,0 +1,27 @@
+package com.nhnacademy.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbUtils {
+    public DbUtils(){
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //todo#0 {ip},{database},{username},{password} 설정합니다.
+            connection = DriverManager.getConnection("jdbc:mysql://133.186.241.167:3306/nhn_academy_55","nhn_academy_55","h*BkDY*BIPnJb7MA");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        return connection;
+    }
+
+}
